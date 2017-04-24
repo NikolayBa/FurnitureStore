3a5c0c5e10f3cd39dd1b0f2b9e387ab0ed80707c
Nikolay Babev <nikolaybabev@gmail.com>
2017-04-25 01:18:31 +0300

    skeleton and first task

--- js/script.js
+++ js/script.js
@@ -1,1 +1,31 @@
-.
+var people = [  {name:"John Doe1", address: "Miami 87" , phonenumber: 088861235 },
+                {name:"John Doe2", address: "Miami 87" , phonenumber: 08886123 },
+                {name:"John Doe3", address: "Miami 87" , phonenumber: 08886123 },
+                {name:"John Doe4", address: "Miami 87" , phonenumber: 08886123 },
+                {name:"John Doe5", address: "Miami 87" , phonenumber: 08886123 }];
+
+var randomId = Math.floor(Math.random() * 5);
+
+var purchase = [ {name:"Sofa", description: "Warm and Soft Sofa" , date: "03/23/2015" },
+                 {name:"Wardrobe", description: "Warm and Soft Sofa" , date: "03/23/2015" },
+                 {name:"Lamp", description: "Warm and Soft Sofa" , date: "03/23/2015" },
+                 {name:"Desk", description: "Warm and Soft Sofa" , date: "03/23/2015" },
+                 {name:"Chair", description: "Warm and Soft Sofa" , date: "03/23/2015" }];
+
+
+$( document ).ready(function() {
+    $("#customerinfo").html("Customer Name : " + people[randomId].name + 
+                            " </br> Customer Address : " + people[randomId].address + 
+                            " </br> Phone Number : " + people[randomId].phonenumber);
+
+    $("#items").empty();
+    $.each( purchase, function( index, value ) {
+         $("#items").append("item name : " + value.name + 
+                            " </br> Description: " + value.description + 
+                            "  </br>  Purchased on : " + value.date + " </br> ");  
+    });       
+
+                             
+});
+
+
