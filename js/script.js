e3e9aa10ae18df547e23a18c3e3afeedd9f41417
Nikolay Babev <nikolaybabev@gmail.com>
2017-04-26 00:58:06 +0300

    nytimes api fetch

--- js/script.js
+++ js/script.js
@@ -1,8 +1,8 @@
-var people = [  {name:"John Doe1", address: "Miami 87" , phonenumber: 088861235 },
-                {name:"John Doe2", address: "Miami 87" , phonenumber: 08886123 },
-                {name:"John Doe3", address: "Miami 87" , phonenumber: 08886123 },
-                {name:"John Doe4", address: "Miami 87" , phonenumber: 08886123 },
-                {name:"John Doe5", address: "Miami 87" , phonenumber: 08886123 }];
+var people = [  {name:"John Doe1", address: "Miami 87" , phonenumber: 088861235, city: "Stockholm" },
+                {name:"John Doe2", address: "Miami 87" , phonenumber: 08886123, city: "Gothenburg" },
+                {name:"John Doe3", address: "Miami 87" , phonenumber: 08886123, city: "Uppsala" },
+                {name:"John Doe4", address: "Miami 87" , phonenumber: 08886123, city: "Lund" },
+                {name:"John Doe5", address: "Miami 87" , phonenumber: 08886123, city: "Halmstad" }];
 
 var randomId = Math.floor(Math.random() * 5);
 
@@ -14,10 +14,11 @@ var purchase = [ {name:"Sofa", description: "Warm and Soft Sofa" , date: "03/23/
 
 
 $( document ).ready(function() {
+    //CUSTOMER INFO
     $("#customerinfo").html("Customer Name : " + people[randomId].name + 
                             " </br> Customer Address : " + people[randomId].address + 
                             " </br> Phone Number : " + people[randomId].phonenumber);
-
+    //ITEMS
     $("#items").empty();
     $.each( purchase, function( index, value ) {
          $("#items").append("item name : " + value.name + 
@@ -25,6 +26,37 @@ $( document ).ready(function() {
                             "  </br>  Purchased on : " + value.date + " </br> ");  
     });       
 
+
+    //NEWS
+    var cityStr  = people[randomId].city;
+    var nyTimesUrl = "https://api.nytimes.com/svc/search/v2/articlesearch.json";
+    nyTimesUrl += '?' + $.param({
+        'api-key': "8c6291b560ce44c383e49265a76bb35d",
+        'q': cityStr
+    });
+
+     $.getJSON(nyTimesUrl, function (data){
+
+        var docArticles = data.response.docs;
+        var items = [];
+
+        $.each( docArticles, function( key, val ) {
+            items.push( '<li class=article> <a href =' + docArticles[key].web_url + '>' +  docArticles[key].headline.main + '</a><p>' + docArticles[key].snippet + '</p></li>' );
+        });
+
+        $( "<ul/>", {
+           "id": "nytimes-articles",
+           "class": "article-list",
+           html: items.join( "" )
+       }).appendTo("#news");
+
+    }).fail(function() {
+        $nytHeaderElem.text("New York times articles could not be loaded");
+    });
+
+
+
+    
                              
 });
 
